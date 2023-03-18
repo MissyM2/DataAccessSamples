d94479c5b496b4e6d528f3365557c0fc59bc9edf
missymaloney <fdmaloney@mac.com>
2023-03-18 11:53:47 -0400

    first commit

---
+++ DataAccessSamples/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace DataAccessSamples;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
+
---
+++ DataAccessSamples/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace DataAccessSamples;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
+
